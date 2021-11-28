ccf8f9b4eee7e839c2f399b261a5eebe9a9a9e99
Adankanych <67391574+Adankanych@users.noreply.github.com>
2021-11-28 13:38:20 -0500

    Project

--- week 4/src/index.js
+++ week 4/src/index.js
@@ -25,21 +25,8 @@ let currentDay = days[currentTime.getDay()];
 let h3 = document.querySelector("h3");
 h3.innerHTML = `${currentDay} ${currentHour}:${currentMintues}`;
 
-// Feature #2: add search engine when searching for a city.
-//Display the city name on the page after user submits the form
-
-function searchCity(event) {
-  event.preventDefault();
-  let city = document.querySelector("#exampleWeather");
-  let h1 = document.querySelector("h1");
-  h1.innerHTML = `Weather in ${city.value}`;
-}
-
-let form = document.querySelector("form");
-form.addEventListener("submit", searchCity);
-
 //Bonus Feature: add link to convert Celsius to Fahrenheit
-function convertToFarenheit(event) {
+/*function convertToFarenheit(event) {
   event.preventDefault();
   let mainTemperature = document.querySelector("#main-temperature");
   mainTemperature.innerHTML = "80";
@@ -55,4 +42,42 @@ function convertToCelsius(event) {
 }
 
 let toCelsius = document.querySelector("#celsius-link");
-toCelsius.addEventListener("click", convertToCelsius);
+toCelsius.addEventListener("click", convertToCelsius); */
+
+function showWeather(response) {
+  console.log(response);
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#main-temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+
+function findCity(city) {
+  let apiKey = "8f92bcf1448991b5fa444b563468f01b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+function handleSubmitEvent(event) {
+  event.preventDefault();
+  let city = document.querySelector("#exampleWeather").value;
+  findCity(city);
+}
+
+function findLocation(position) {
+  let apiKey = "8f92bcf1448991b5fa444b563468f01b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+function getCurrentPosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(findLocation);
+}
+let currentLocation = document.querySelector("#current");
+currentLocation.addEventListener("click", getCurrentPosition);
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmitEvent);
+
+findCity("Grove City");
